06b1149f4f6ff5ff8579552a513f976b9034966c
vbalaji215-dev <vbalaji215@outlook.com>
2022-10-06 11:54:01 -0400

    ZEUS-151 stored the response payload to the detail table

--- src/main/java/com/brihaspathee/zeus/broker/producer/AccountValidationResultProducer.java
+++ src/main/java/com/brihaspathee/zeus/broker/producer/AccountValidationResultProducer.java
@@ -1,6 +1,11 @@
 package com.brihaspathee.zeus.broker.producer;
 
+import com.brihaspathee.zeus.domain.entity.PayloadTracker;
+import com.brihaspathee.zeus.domain.entity.PayloadTrackerDetail;
+import com.brihaspathee.zeus.helper.interfaces.PayloadTrackerDetailHelper;
 import com.brihaspathee.zeus.validator.AccountValidationResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -20,10 +25,32 @@ import org.springframework.stereotype.Component;
 public class AccountValidationResultProducer {
 
     /**
-     * Send the account validation result back to member management servuce
+     * Object mapper that can covert the object into a string
+     */
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Payload tracker detail helper instance to create the payload tracker detail record
+     */
+    private final PayloadTrackerDetailHelper payloadTrackerDetailHelper;
+
+    /**
+     * Send the account validation result back to member management service
+     * @param payloadTracker
      * @param accountValidationResult
      */
-    public void sendAccountValidationResult(AccountValidationResult accountValidationResult){
+    public void sendAccountValidationResult(PayloadTracker payloadTracker,
+                                            AccountValidationResult accountValidationResult) throws JsonProcessingException {
         log.info("Inside the account validation producer:{}", accountValidationResult);
+        // Convert the payload as String
+        String valueAsString = objectMapper.writeValueAsString(accountValidationResult);
+
+        // Store the response in the detail table
+        PayloadTrackerDetail payloadTrackerDetail = PayloadTrackerDetail.builder()
+                .payloadTracker(payloadTracker)
+                .responseTypeCode("RESPONSE")
+                .responsePayload(valueAsString)
+                .build();
+        payloadTrackerDetailHelper.createPayloadTrackerDetail(payloadTrackerDetail);
     }
 }

424404a77a69f5c1a7b8b3df35e108c5d51570a6
vbalaji215-dev <vbalaji215@outlook.com>
2022-10-10 16:41:05 -0400

    ZEUS-105 added the attribute response payload id

--- src/main/resources/db/migration/V1__init_table_creation.sql
+++ src/main/resources/db/migration/V1__init_table_creation.sql
@@ -12,21 +12,22 @@ CREATE TABLE IF NOT EXISTS `validationdb`.`payload_tracker` (
 ENGINE = InnoDB
 COMMENT = 'This table tracks all the inbound and outbound payloads in the validation service';
 CREATE TABLE IF NOT EXISTS `validationdb`.`payload_tracker_detail` (
-  `payload_tracker_detail_sk` VARCHAR(36) NOT NULL COMMENT 'Primary key of the table',
-  `payload_tracker_sk` VARCHAR(36) NOT NULL COMMENT 'The foreign key of the payload tracker table',
-  `response_type_code` VARCHAR(45) NOT NULL COMMENT 'The type of response received or sent. e.g. ACK, RESULT etc',
-  `response_payload` LONGTEXT NOT NULL,
-  `created_date` DATETIME NULL COMMENT 'The date when the record was created',
-  `updated_date` DATETIME NULL COMMENT 'The date when the record was updated',
-  PRIMARY KEY (`payload_tracker_detail_sk`),
-  INDEX `payload_tracker_fk_idx` (`payload_tracker_sk` ASC) VISIBLE,
-  CONSTRAINT `payload_tracker_fk`
+    `payload_tracker_detail_sk` VARCHAR(36) NOT NULL COMMENT 'Primary key of the table',
+    `payload_tracker_sk` VARCHAR(36) NOT NULL COMMENT 'The foreign key of the payload tracker table',
+    `response_type_code` VARCHAR(45) NOT NULL COMMENT 'The type of response received or sent. e.g. ACK, RESULT etc',
+    `response_payload` LONGTEXT NOT NULL,
+    `response_payload_id` VARCHAR(45) NULL,
+    `created_date` DATETIME NULL COMMENT 'The date when the record was created',
+    `updated_date` DATETIME NULL COMMENT 'The date when the record was updated',
+    PRIMARY KEY (`payload_tracker_detail_sk`),
+    INDEX `payload_tracker_fk_idx` (`payload_tracker_sk` ASC) VISIBLE,
+    CONSTRAINT `payload_tracker_fk`
     FOREIGN KEY (`payload_tracker_sk`)
     REFERENCES `validationdb`.`payload_tracker` (`payload_tracker_sk`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-COMMENT = 'The payload tracker detail table, that tracks all the responses received for an outbound payload and all the responses sent for an inbound payload';
+    ENGINE = InnoDB
+    COMMENT = 'The payload tracker detail table, that tracks all the responses received for an outbound payload and all the responses sent for an inbound payload';
 CREATE TABLE IF NOT EXISTS `validationdb`.`rule_category` (
     `rule_category_sk` VARCHAR(36) NOT NULL COMMENT 'The primary key of the table',
     `rule_category_id` VARCHAR(50) NOT NULL COMMENT 'The unique Id of the category',

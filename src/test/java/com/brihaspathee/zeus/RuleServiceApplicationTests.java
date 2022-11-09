14eb59a21a9acb2f72ab70834fd59a74269b12cb
vbalaji215-dev <vbalaji215@outlook.com>
2022-11-09 13:11:13 -0500

    Initial add

---
+++ src/test/java/com/brihaspathee/zeus/RuleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
